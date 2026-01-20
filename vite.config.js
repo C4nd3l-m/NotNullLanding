a1827415e5f56585ed25c6d7405022428a22371f
candela-oberstaff <candela@oberstaff.com>
2026-01-19 23:49:23 -0300

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import htmlInject from 'vite-plugin-html-inject';
 
 export default defineConfig({
   root: 'src',
+  base: './',
   publicDir: '../public', // We might not need this if we put everything in src/assets, but good practice
   build: {
     outDir: '../dist',
